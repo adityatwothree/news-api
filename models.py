1eaa55c392ef6ed76947af7eb5615d0db4c8f115
Aditya Kumar Seth <sethaditya35@gmail.com>
2025-09-14 21:35:30 +0530

    misc: cleanup unused import

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from typing import List, Optional, Dict, Any
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field
 from enum import Enum
 
 
